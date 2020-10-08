e26ca3963656e37ace0c26609f6ee5633fd463ea
Mavar <simakov_matvey@mail.ru>
2020-10-08 20:26:27 +0300

    Обмен сообщениями и контактами

--- CsharpBot/CROCK_CsharpBot/Bot.cs
+++ CsharpBot/CROCK_CsharpBot/Bot.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace CROCK_CsharpBot
 {
@@ -18,9 +19,50 @@ namespace CROCK_CsharpBot
 
         private void MessagProcessor(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            client.SendTextMessageAsync(e.Message.Chat.Id, "Hi There");
-            Console.WriteLine(e.Message.Text);
+            switch (e.Message.Type)
+            {
+                case Telegram.Bot.Types.Enums.MessageType.Contact:
+                    string phone = e.Message.Contact.PhoneNumber;
+                    client.SendTextMessageAsync(e.Message.Chat.Id, $"Твой телефон: {phone}");
+                    break;
+                case Telegram.Bot.Types.Enums.MessageType.Text:
+                    if (e.Message.Text.Substring(0, 1) == "/")
+                    {
+                        CommadProcessor(e.Message);
+                    }
+                    else
+                    {
+                        client.SendTextMessageAsync(e.Message.Chat.Id, "Hi There");
+                        client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты сказал мне: {e.Message.Text}");
+                        Console.WriteLine(e.Message.Text);
+                    }
+                    break;
+                default:
+                    client.SendTextMessageAsync(e.Message.Chat.Id, "Hi There");
+                    client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты прислал мне: {e.Message.Type}");
+                    Console.WriteLine(e.Message.Type);
+                    break;
+
+            }
+
             // throw new NotImplementedException();
+        }
+        private void CommadProcessor(Telegram.Bot.Types.Message message)
+        {
+            string comand = message.Text.Substring(1).ToLower();
+            switch (comand)
+            {
+                case "start":
+                    var button = new KeyboardButton("Поделимь телефоном");
+                    button.RequestContact = true;
+                    var array = new KeyboardButton[] { button };
+                    var reply = new ReplyKeyboardMarkup(array, true, true );
+                    client.SendTextMessageAsync(message.Chat.Id, $"Привет, {message.Chat.FirstName} скажи мне свой телефон: ");
+                    break;
+                default:
+                    client.SendTextMessageAsync(message.Chat.Id, $"Я пока не понимаю команду: {comand}");
+                    break;
+            }
         }
 
         public void Run()
